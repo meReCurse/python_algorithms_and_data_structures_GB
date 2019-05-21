8f34f73be596b950a58e40b308ff70313b7e6659
meReCurse <georgykorchagin@mail.ru>
2019-05-21 18:05:32 +0300

    hw1_fix

--- 1/5.py
+++ 1/5.py
@@ -8,12 +8,17 @@
 def get_position(x):
     return ord(x) - 96
 
-
 first_letter = input('первая буква: ')
 second_letter = input('вторая буква: ')
+
 first_letter_pos = get_position(first_letter)
 second_letter_pos = get_position(second_letter)
-between_quantity = second_letter_pos - first_letter_pos - 1
+
+if first_letter_pos < second_letter_pos:
+	between_quantity = second_letter_pos - first_letter_pos - 1	
+else:
+	between_quantity = first_letter_pos - second_letter_pos - 1
+
 print(f'позиция буквы {first_letter} - {first_letter_pos}')
 print(f'позиция буквы {second_letter} - {second_letter_pos}')
 print(f'между ними символов - {between_quantity}')
